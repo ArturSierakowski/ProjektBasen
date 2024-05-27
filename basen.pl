3936f903242895cb3273d228a84a393ea3532dfb
ArturSierakowski <115699855+ArturSierakowski@users.noreply.github.com>
2024-05-27 23:51:57 +0200

    Update basen.pl

--- basen.pl
+++ basen.pl
@@ -1,25 +1,24 @@
 :- dynamic rezerwacja/4.
 
-% Ladowanie pliku z rezerwacjami
+% Ładowanie pliku z rezerwacjami
 :- [rezerwacje].
 
-% Ladowanie pliku graficznego interfejsu
+% Ładowanie pliku GUI
 :- [basen_gui].
 
-% Spis funkcji glównych i pomocniczych:
-% 1. Glówne funkcje:
+% Spis funkcji głównych i pomocniczych:
+% 1. Główne funkcje:
 %    - dodaj_rezerwacje/4
 %    - dostepnosc/3
 %    - anuluj_rezerwacje/4
 %    - wyswietl_rezerwacje/0
-%    - filtruj_rezerwacje_uzytkownika/2
-%    - filtruj_rezerwacje_daty/2
-%    - filtruj_rezerwacje_toru/2
 %    - wyszukaj_rezerwacje_uzytkownika/1
+%    - wyszukaj_rezerwacje_daty/1
+%    - wyszukaj_rezerwacje_toru/1
 %    - edytuj_rezerwacje/5
 %    - dodaj_rezerwacje_cykliczne/5
 %    - raport_liczba_rezerwacji/0
-%    - najczesciej_rezerwowane_godziny/1
+%    - najczesciej_rezerwowane_godziny/0
 % 2. Funkcje pomocnicze:
 %    - parse_date/2
 %    - format_date/2
@@ -32,7 +31,7 @@
 
 % --------------------------  Funkcje pomocnicze --------------------------
 
-% Pomocnicza funkcja do parsowania daty i weryfikacji poprawnosci
+% Pomocnicza funkcja do parsowania daty i weryfikacji poprawności
 parse_date(Atom, date(Year, Month, Day)) :-
     atomic_list_concat([Y, M, D], '-', Atom),
     atom_number(Y, Year),
@@ -42,7 +41,7 @@ parse_date(Atom, date(Year, Month, Day)) :-
     days_in_month(Year, Month, MaxDay),
     between(1, MaxDay, Day).
 
-% Funkcja do okreslania liczby dni w miesiacu, uwzgledniajaca lata przestepne
+% Funkcja do określania liczby dni w miesiącu, uwzględniająca lata przestępne
 days_in_month(Year, 2, 29) :-
     Year mod 4 =:= 0,
     (Year mod 100 =\= 0 ; Year mod 400 =:= 0), !.
@@ -74,12 +73,12 @@ zapisz_rezerwacje :-
     listing(rezerwacja/4),
     told.
 
-% Liczba rezerwacji dla kazdego toru
+% Liczba rezerwacji dla każdego toru
 raport_liczba_rezerwacji_tor(Tor, Liczba) :-
     findall(Tor, rezerwacja(_, Tor, _, _), Lista),
     length(Lista, Liczba).
 
-% Pomocnicza funkcja do liczenia wystapien
+% Pomocnicza funkcja do liczenia wystąpień
 clumped([], []).
 clumped([X|Xs], [X-N|Ys]) :-
     clump(X, Xs, Xs1, 1, N),
@@ -92,48 +91,39 @@ clump(X, [X|Xs], Ys, K, N) :-
     K1 is K + 1,
     clump(X, Xs, Ys, K1, N).
 
-% -----------------------------  G³ówne funkcje -----------------------------
-
+% -----------------------------  Główne funkcje -----------------------------
 
 dodaj_rezerwacje(Uzytkownik, Tor, Data, Godzina) :-
     integer(Tor), between(1, 8, Tor),             % Walidacja toru
     parse_date(Data, _),                          % Walidacja daty
     integer(Godzina), between(8, 20, Godzina),    % Walidacja godziny
-    \+ rezerwacja(_, Tor, Data, Godzina),         % Sprawdzanie, czy tor nie jest ju¿ zajêty
+    \+ rezerwacja(_, Tor, Data, Godzina),         % Sprawdzanie, czy tor nie jest już zajęty
     assertz(rezerwacja(Uzytkownik, Tor, Data, Godzina)),
     zapisz_rezerwacje.
 
-
 dostepnosc(Tor, Data, Godzina) :-
     \+ rezerwacja(_, Tor, Data, Godzina).
-
 
 anuluj_rezerwacje(Uzytkownik, Tor, Data, Godzina) :-
     retract(rezerwacja(Uzytkownik, Tor, Data, Godzina)),
     zapisz_rezerwacje.
 
-
 wyswietl_rezerwacje :-
     findall((Uzytkownik, Tor, Data, Godzina), rezerwacja(Uzytkownik, Tor, Data, Godzina), Rezerwacje),
     writeln(Rezerwacje).
 
-
-filtruj_rezerwacje_uzytkownika(Uzytkownik, Rezerwacje) :-
-    findall((Uzytkownik, Tor, Data, Godzina), rezerwacja(Uzytkownik, Tor, Data, Godzina), Rezerwacje).
-
-
-filtruj_rezerwacje_daty(Data, Rezerwacje) :-
-    parse_date(Data, _),                          % Walidacja daty
-    findall((Uzytkownik, Tor, Data, Godzina), rezerwacja(Uzytkownik, Tor, Data, Godzina), Rezerwacje).
-
-
-filtruj_rezerwacje_toru(Tor, Rezerwacje) :-
-    integer(Tor), between(1, 8, Tor),             % Walidacja toru
-    findall((Uzytkownik, Tor, Data, Godzina), rezerwacja(Uzytkownik, Tor, Data, Godzina), Rezerwacje).
-
-
 wyszukaj_rezerwacje_uzytkownika(Uzytkownik) :-
     findall((Tor, Data, Godzina), rezerwacja(Uzytkownik, Tor, Data, Godzina), Rezerwacje),
+    writeln(Rezerwacje).
+
+wyszukaj_rezerwacje_daty(Data) :-
+    parse_date(Data, _),                          % Walidacja daty
+    findall((Uzytkownik, Tor, Godzina), rezerwacja(Uzytkownik, Tor, Data, Godzina), Rezerwacje),
+    writeln(Rezerwacje).
+
+wyszukaj_rezerwacje_toru(Tor) :-
+    integer(Tor), between(1, 8, Tor),             % Walidacja toru
+    findall((Uzytkownik, Data, Godzina), rezerwacja(Uzytkownik, Tor, Data, Godzina), Rezerwacje),
     writeln(Rezerwacje).
 
 
@@ -151,7 +141,7 @@ edytuj_rezerwacje(Uzytkownik, Index, NowyTor, NowaData, NowaGodzina) :-
         fail
     ).
 
-% Dodaj rezerwacje cykliczne (co tydzien przez n tygodni)
+% Dodaj rezerwacje cykliczne (co tydzień przez n tygodni)
 dodaj_rezerwacje_cykliczne(Uzytkownik, Tor, Data, Godzina, Tygodnie) :-
     integer(Tor), between(1, 8, Tor),             % Walidacja toru
     parse_date(Data, ParsedDate),                 % Walidacja daty
@@ -168,16 +158,14 @@ dodaj_rezerwacje_cykliczne(Uzytkownik, Tor, Data, Godzina, Tygodnie) :-
     ;   fail
     ).
 
-% Raport liczby rezerwacji dla wszystkich torów
 raport_liczba_rezerwacji :-
     forall(between(1, 8, Tor),
            (raport_liczba_rezerwacji_tor(Tor, Liczba),
             format('Tor ~w: ~w rezerwacji~n', [Tor, Liczba]))).
 
-najczesciej_rezerwowane_godziny(Godziny) :-
+najczesciej_rezerwowane_godziny :-
     findall(Godzina, rezerwacja(_, _, _, Godzina), ListaGodzin),
     msort(ListaGodzin, ListaPosortowana),
     clumped(ListaPosortowana, GodzinyPosortowane),
     sort(2, @>=, GodzinyPosortowane, Godziny),
     writeln(Godziny).
-
